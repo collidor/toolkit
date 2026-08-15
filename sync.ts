a724a07308588c48d6a8c2fcf3d0c1ef17e8bc1f
Alykam Burdzaki <alykam@gmail.com>
2026-08-15 14:11:52 +0200

    feat: add automated semantic-release pipeline for npm and jsr

--- sync.ts
+++ sync.ts
@@ -1,22 +1,6 @@
-const denoJsonText = await Deno.readTextFile("deno.json");
-const packageJsonText = await Deno.readTextFile("package.json");
-
-const denoJson = JSON.parse(denoJsonText);
-const packageJson = JSON.parse(packageJsonText);
+const denoJson = JSON.parse(await Deno.readTextFile("deno.json"));
+const packageJson = JSON.parse(await Deno.readTextFile("package.json"));
 
 denoJson.version = packageJson.version;
 
-await Promise.all(
-  Object.keys(denoJson.imports).map(async (key) => {
-    if (key.startsWith("@collidor/")) {
-      const pkgName = key.replace("@collidor/", "");
-      const pkgPackageJsonText = await Deno.readTextFile(
-        `../${pkgName}/package.json`,
-      );
-      const pkgPackageJson = JSON.parse(pkgPackageJsonText);
-      denoJson.imports[key] = `jsr:${key}@^${pkgPackageJson.version}`;
-    }
-  }),
-);
-
-await Deno.writeTextFile("deno.json", JSON.stringify(denoJson, null, 4));
+await Deno.writeTextFile("deno.json", JSON.stringify(denoJson, null, 4) + "\n");
